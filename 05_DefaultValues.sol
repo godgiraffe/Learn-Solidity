a3ccfcb7f382c09fab7e8cebf7dd65d37e54f32a
LL <godgiraffe1218@gmail.com>
2023-03-26 12:15:55 +0800

    DefaultValues

---
+++ 05_DefaultValues.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract DefaultValues {
+    bool public b;          // false
+    uint public u;          // 0
+    int public i;           // 0
+    address public addr;    // 0x0000000000000000000000000000000000000000
+    bytes public bs;        // 0x
+    bytes32 public bs32;    // 0x0000000000000000000000000000000000000000000000000000000000000000
+}
